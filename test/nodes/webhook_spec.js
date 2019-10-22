080f03179f9a6638d9af9c001524f4ca5c66345d
Rafal Kukulski <rafal.kukulski@gmail.com>
2019-10-23 01:29:45 +0200

    feat: add webhook node

---
+++ test/nodes/webhook_spec.js
@@ -0,0 +1,22 @@
+var helper = require('node-red-node-test-helper');
+var shared = require('./shared.js');
+var webhookNode = require('../../nodes/webhook.js');
+
+helper.init(require.resolve('node-red'));
+
+describe('webhook node', function() {
+  beforeEach(function(done) {
+    helper.startServer(done);
+  });
+
+  afterEach(function(done) {
+    helper.unload();
+    helper.stopServer(done);
+  });
+
+  shared.shouldLoadCorrectly(webhookNode, 'webhook');
+
+  it('should create POST endpoint');
+  it('should remove POST endpoint on close');
+  it('should send received payload to the next node');
+});
