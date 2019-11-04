8937fd43eb906de87be7819c29afff7dac352d87
Rafal Kukulski <rafal.kukulski@gmail.com>
2019-11-04 09:51:03 +0100

    feat: add say node

---
+++ test/nodes/say_spec.js
@@ -0,0 +1,34 @@
+var should = require('should');
+var helper = require('node-red-node-test-helper');
+var shared = require('./shared.js');
+var sayNode = require('./../../nodes/say.js');
+var res = require('./mocks.js').res;
+var fs = require('fs');
+
+helper.init(require.resolve('node-red'));
+
+describe('say node', function() {
+  beforeEach(function(done) {
+    helper.startServer(done);
+  });
+
+  afterEach(function(done) {
+    helper.unload();
+    helper.stopServer(done);
+  });
+
+  shared.shouldLoadCorrectly(sayNode, 'say');
+
+  it('should respond with proper XML', function(done) {
+    var flow = [{ id: 'n1', type: 'say', text: 'Hello World', output: 'off' }];
+    var xml = fs.readFileSync('test/resources/xml/say.xml', 'utf8');
+    helper.load(sayNode, flow, function() {
+      var n1 = helper.getNode('n1');
+      n1.on('input', function(msg) {
+        should(msg.res._res.responseBody).be.eql(xml);
+        done();
+      });
+      n1.receive({ payload: '<call data>', res: res });
+    });
+  });
+});
