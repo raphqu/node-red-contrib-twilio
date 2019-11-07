bff2c093f1f8c2df93f1be62b8d2cc34626215ac
Rafal Kukulski <rafal.kukulski@gmail.com>
2019-11-07 23:04:10 +0100

    feat: Handle status callbacks in dial node

--- nodes/dial.js
+++ nodes/dial.js
@@ -1,14 +1,22 @@
 module.exports = function(RED) {
   'use strict';
+  var url = require('url');
+  var bodyParser = require('body-parser');
   var VoiceResponse = require('twilio').twiml.VoiceResponse;
 
   function DialNode(config) {
     RED.nodes.createNode(this, config);
     this.numbers = config.numbers;
+    this.statusCallback = config.statusCallback;
+    this.outputs = config.outputs;
+    this.callbackUrl = '/callback-' + randomId();
+    this.method = 'post';
     var node = this;
 
     node.on('input', function(msg) {
       var response;
+      var absoluteCallbackUrl;
+
       if (msg.payload.twilio) {
         response = msg.payload.twilio;
       } else {
@@ -16,12 +24,57 @@ module.exports = function(RED) {
       }
       var dial = response.dial();
       for (var number of node.numbers) {
-        dial.number(number.number);
+        if (node.statusCallback) {
+          absoluteCallbackUrl =
+            absoluteCallbackUrl || url.resolve(node.context().global.get('baseUrl'), node.callbackUrl);
+          dial.number(
+            {
+              statusCallbackEvent: 'initiated ringing answered completed',
+              statusCallback: absoluteCallbackUrl,
+            },
+            number.number
+          );
+        } else {
+          dial.number(number.number);
+        }
       }
       msg.payload = response.toString();
       msg.res._res.set('Content-Type', 'application/xml');
       msg.res._res.status(200).send(msg.payload);
     });
+
+    function randomId() {
+      return Math.floor(Math.random() * 1000000);
+    }
+
+    this.errorHandler = function(err, req, res) {
+      node.warn(err);
+      res.sendStatus(500);
+    };
+
+    this.callback = function(req, res) {
+      var msgid = RED.util.generateId();
+      res._msgid = msgid;
+      var msg = { _msgid: msgid, req: req, res: { _res: res }, payload: req.body };
+      res.sendStatus(200);
+      node.send(msg);
+    };
+
+    var maxApiRequestSize = RED.settings.apiMaxLength || '5mb';
+    var urlencParser = bodyParser.urlencoded({ limit: maxApiRequestSize, extended: true });
+
+    if (node.statusCallback) {
+      RED.httpNode.post(this.callbackUrl, urlencParser, this.callback, this.errorHandler);
+
+      this.on('close', function() {
+        var node = this;
+        RED.httpNode._router.stack.forEach(function(route, i, routes) {
+          if (route.route && route.route.path === node.callbackUrl && route.route.methods[node.method]) {
+            routes.splice(i, 1);
+          }
+        });
+      });
+    }
   }
   RED.nodes.registerType('dial', DialNode);
 };
--- test/nodes/dial_spec.js
+++ test/nodes/dial_spec.js
@@ -82,4 +82,22 @@ describe('dial node', function() {
       n1.receive({ payload: '<call data>', res: res });
     });
   });
+
+  it('should respond with proper XML for active statusCallback', function(done) {
+    var flow = [{ id: 'n1', type: 'dial', numbers: [{ number: '4915799912345' }], statusCallback: true, outputs: 1 }];
+    var xml = fs.readFileSync('test/resources/xml/dial_with_statusCallback.xml', 'utf8');
+    helper.load(dialNode, flow, function() {
+      var n1 = helper.getNode('n1');
+      n1.context().global.set('baseUrl', 'http://example.com');
+      n1.on('input', function(msg) {
+        should(msg.res._res.responseBody).be.eql(xml.replace('${callbackUrl}', n1.callbackUrl));
+        done();
+      });
+      n1.receive({ payload: '<call data>', res: res });
+    });
+  });
+
+  it('should create status callback');
+  it('should remove status callback on close');
+  it('should send received status callback payload to the next node');
 });
