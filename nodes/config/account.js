a2a012aa4f83af30972c497931baa2e965803853
Rafal Kukulski <rafal.kukulski@gmail.com>
2019-11-15 12:59:41 +0100

    feat: Add hangup-call node

---
+++ nodes/config/account.js
@@ -0,0 +1,11 @@
+module.exports = function(RED) {
+  function AccountNode(config) {
+    RED.nodes.createNode(this, config);
+  }
+  RED.nodes.registerType('account', AccountNode, {
+    credentials: {
+      sid: { type: 'text' },
+      token: { type: 'password' },
+    },
+  });
+};
---
+++ nodes/hangup-call.js
@@ -0,0 +1,15 @@
+module.exports = function(RED) {
+  'use strict';
+
+  function HangupCallNode(config) {
+    RED.nodes.createNode(this, config);
+    this.account = RED.nodes.getNode(config.account);
+    var node = this;
+    var client = require('twilio')(node.account.credentials.sid, node.account.credentials.token);
+
+    node.on('input', function(msg) {
+      client.calls(msg.payload.CallSid).update({ status: 'completed' });
+    });
+  }
+  RED.nodes.registerType('hangup-call', HangupCallNode);
+};
